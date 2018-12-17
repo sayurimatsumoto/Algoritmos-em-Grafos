4ee9eb77294db21b1cd11bf3b8447628bbc47554
sayurimatsumoto <isabelasassa@hotmail.com>
2018-12-16 22:44:54 -0200

    quase lá

--- t5/t5-isabelasm.c
+++ t5/t5-isabelasm.c
@@ -1,75 +1,109 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-typedef struct listaAdj {
-	int *adj;
-	int numAdj;
-	int myTime;
-	int visit;
-	int f;
-}ListaAdj;
-int buscaProfundidade(ListaAdj grafo[], int n, int u, int time, int *maior);
-int main(){
-	int n, m, i, u, v;
-	while(1){
-		int max = 0;
-		scanf("%d %d", &n, &m);
-		if(m == 0 && n == 0){
-			return 0;
-		}
-		ListaAdj *grafo =  (ListaAdj *) calloc(n,sizeof(ListaAdj));
-		for(i = 0; i < n; i++){
-			grafo[i].adj = (int *) calloc (n+1,sizeof(int));
-			grafo[i].numAdj = 0;
-			grafo[i].visit = 0;
-			grafo[i].f = 0;
-			scanf("%d", &grafo[i].myTime);
-		}
-		for(i = 0; i < m; i++){
-			scanf("%d %d", &u, &v);
-			grafo[u].adj[grafo[u].numAdj] = v;
-			grafo[u].numAdj++;
 
+typedef struct no{
+	int num;
+	int custo;
+}No;
+typedef struct listaAdj {
+	No *adj;
+	int numAdj;
+	int custo;
+	int dist;
+	int s;
+}ListaAdj;
+
+int dijkstra(ListaAdj grafo[], int *minCusto, int n);
+int menorDist(ListaAdj grafo[], int n);
+
+int main(){
+	int n, m, k, i, u, v, custo;
+
+	scanf("%d %d %d", &n, &m, &k);
+	
+	ListaAdj *grafo =  (ListaAdj *) malloc(n * sizeof(ListaAdj));
+
+	int minCusto[n];
+
+	for(i = 0; i < n; i++){
+		grafo[i].adj = (No *) malloc (n+1 * sizeof(No));
+		grafo[i].numAdj = 0;
+		if(i < k){
+			grafo[i].dist = 0;
+			grafo[i].s = 1;
+			minCusto[i] = i;
+		}else{
+			grafo[i].dist = -1;
+			grafo[i].s = 1;
+			minCusto[i] = -1;
 		}
-		for(i = 0; i < n; i++){
-			if(grafo[i].visit == 0){
-				buscaProfundidade(grafo, n, i, 0, &max);
-			}
-		}
-		/*for(i = 0; i < n; i++){
-			printf("%d ", grafo[i].f);
-		}*/
-		printf("%d\n", max);
-		for(i = 0; i < n; i++){
-			free(grafo[i].adj);
-		}
-		free(grafo);
+	}	
+
+	for(i = 0; i < m; i++){
+		// de u a v
+		scanf("%d %d %d", &u, &v, &custo);
+		grafo[u].adj[grafo[u].numAdj].num = v;
+		grafo[u].numAdj++;
+		grafo[u].adj[grafo[u].numAdj].custo = custo;
+		// de v a u
+		grafo[v].adj[grafo[v].numAdj].num = u;
+		grafo[v].numAdj++;
+		grafo[v].adj[grafo[v].numAdj].custo = custo;
 	}
+
+	dijkstra(grafo, minCusto, n);
+
+	for(i = k; i < n; i++){
+		if(i == k ){
+			printf("%d", minCusto[i]);
+		}else{
+			printf(" %d", minCusto[i]);
+		}
+	}
+	printf("\n");
+	
 	return 0;
 }
-int buscaProfundidade(ListaAdj grafo[], int n, int u, int time, int *maior){
-	int v;
-	grafo[u].visit = 1;
-	//grafo[u].dist = time;
-	time++;
-	int max = grafo[u].myTime;
-	for(int i = 0; i < grafo[u].numAdj ;i++){
-		v = grafo[u].adj[i];
-		if(grafo[v].visit == 0){
-			//grafo[v].pred = u;
-			time = buscaProfundidade(grafo, n, v, time, maior);
-		}
-		if(max < grafo[u].myTime + grafo[v].myTime){
-			max = grafo[u].myTime + grafo[v].myTime;
-		}
 
+int dijkstra(ListaAdj grafo[], int *minCusto, int n){
+	int i, v;
+	int u = menorDist(grafo, n);
+	while(u != -1){
+		//printf("AAAAAAAAAAAA\n");
+		for(i = 0; i < grafo[u].numAdj; i++){
+			v = grafo[u].adj[i].num;
+			if(grafo[v].s == 1){
+				if(grafo[v].dist == -1){
+					grafo[v].dist = grafo[u].dist + grafo[u].adj[v].custo;
+					minCusto[v] = minCusto[u];
+
+				}else if(grafo[v].dist > grafo[u].dist + grafo[u].adj[v].custo){
+					grafo[v].dist = grafo[u].dist + grafo[u].adj[v].custo;
+					minCusto[v] = minCusto[u];
+
+				}else if(grafo[v].dist == grafo[u].dist + grafo[u].adj[v].custo && minCusto[v] > minCusto[u]){
+						minCusto[v] = minCusto[u];
+				}
+			}
+		}
+		u = menorDist(grafo, n);
 	}
-	time++;
-	grafo[u].visit = 2;
-	grafo[u].f = time;
-	grafo[u].myTime = max;
-	if(*maior < max){
-		*maior = max;
+}
+int menorDist(ListaAdj grafo[], int n){
+	int i, min = -1;
+
+	for(i = 0; i < n; i++){
+		if(min == -1){
+			if(grafo[i].s && grafo[i].dist != -1){
+				min = i;
+			}
+		}else if(grafo[i].s && grafo[i].dist != -1 && grafo[i].dist < grafo[min].dist){
+			min = i;
+		}
 	}
-	return time;
+	if(min != -1){
+		grafo[min].s = 0;
+	}
+	return min;
 }
